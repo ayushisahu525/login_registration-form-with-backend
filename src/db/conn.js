7e3fae47636476eb5308a4bc55cbfcd14d156bc3
Ayushi Sahu <ayushisahu525@gmail.com>
2023-07-09 23:06:47 +0530

    changes made

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
+
 mongoose.set('strictQuery', false);
+
+ 
 // mongoose.connect(process.env.MONGO_URL);
 
-mongoose.connect('mongodb://localhost:27017/registration' , 
+
+mongoose.connect(process.env.MONGO_URL, 
 {useNewUrlParser:true , useUnifiedTopology:true}).then( () => console.log("connection successful.."))
 .catch( (err) => console.log(err));
+
